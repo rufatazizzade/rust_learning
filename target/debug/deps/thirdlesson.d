5f71f7ff67c0a79c5666a3b464f91ffee435de85
Rufat Azizzade <azizzruf@fit.cvut.cz>
2026-04-08 01:39:26 +0200

    changed directory settings

---
+++ target/debug/deps/thirdlesson.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\target\debug\deps\thirdlesson.d: src\main.rs
+
+C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\target\debug\deps\thirdlesson.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/thirdlesson.d
@@ -0,0 +1,1 @@
+C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\target\debug\thirdlesson.exe: C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\src\main.rs
